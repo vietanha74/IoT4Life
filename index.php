36f27cb6ee65383dc93ce7d5e8b6b6d596ecdc28
Viet Anh <vietanha74@gmail.com>
2019-11-05 21:56:04 +0700

    ADDD
    ADDD

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
-<!DOCTYPE html>
-<html>
-<body>
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
-<p>This is a paragraph.</p>
-<p>This is a paragraph.</p>
-<p>This is a paragraph.</p>
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
 
-</body>
-</html>
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
